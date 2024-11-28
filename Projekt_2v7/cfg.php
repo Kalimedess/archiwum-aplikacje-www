3125dd34a3ab889be55fd410e1efd231593b089e
Cezary Ignaszewski <69848161+Kalimedess@users.noreply.github.com>
2024-11-28 16:14:25 +0100

    Add files via upload

---
+++ Projekt_2v7/cfg.php
@@ -0,0 +1,14 @@
+<?php
+    $dbhost = 'localhost';
+    $dbuser = 'root';
+    $dbpass = '';
+    $baza = 'moja_strona';
+    $login = 'phpadmin';
+    $pass = '1234';
+
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+    $link = mysqli_connect($dbhost,$dbuser,$dbpass);
+    if(!$link) echo 'Przerwane połączenie';
+    if(!mysqli_select_db($link, $baza)) echo 'nie wybrano bazy';
+
+    ?>
---
+++ Projekt_2v7/contact.php
@@ -0,0 +1,7 @@
+<?php
+	function PokazKontakt(){
+
+	}
+	function WyslijMailNaKontakt()
+	function PrzypomnijHaslo()
+?>
